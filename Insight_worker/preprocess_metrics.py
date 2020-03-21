b61f4a12101a235356bba67fa08bf439dc80bc8c
PRATIK MISHRA <43684300+pratikmishra356@users.noreply.github.com>
2020-03-21 20:41:23 +0530

    Update preprocess_metrics.py

--- Insight_worker/preprocess_metrics.py
+++ Insight_worker/preprocess_metrics.py
@@ -14,3 +14,50 @@ id_value = {'issue-backlog':['issue_id','duration','created_at'],
                            'issue_comments','pull_requests','pull_request_comments','total']}
 
 text = {'message':['msg_id','cntrb_id','msg_text']} #currently there is no metrics avialable for text.
+
+
+df_time_series = pd.DataFrame(index=index)
+for endpoint in self.time_series.items():
+            
+    url = base_url + endpoint
+    logging.info("Hitting endpoint: " + url + "\n")
+    try:
+        data = requests.get(url=url).json()
+    except:
+        data = json.loads(json.dumps(requests.get(url=url).text))
+
+    metric_df = pd.DataFrame.from_records(data)
+    
+    df_time_series = pd.DataFrame(pd.merged(df_time_series,metric_df, how='outer', on='date'))
+    
+    df_time_series.index = pd.to_datetime(df_time_series['date'], utc=True).dt.date
+    
+    
+df_id = pd.DataFrame(index=index)
+for endpoint in self.time_series.items():
+            
+    url = base_url + endpoint
+    logging.info("Hitting endpoint: " + url + "\n")
+    try:
+        data = requests.get(url=url).json()
+    except:
+        data = json.loads(json.dumps(requests.get(url=url).text))
+
+    metric_df = pd.DataFrame.from_records(data)
+    
+    df_id = pd.DataFrame(pd.merged(df_time_series,metric_df, how='outer', on='issue_id'))
+    
+   
+df_msg = pd.DataFrame(index=index)
+for endpoint in self.time_series.items():
+            
+    url = base_url + endpoint
+    logging.info("Hitting endpoint: " + url + "\n")
+    try:
+        data = requests.get(url=url).json()
+    except:
+        data = json.loads(json.dumps(requests.get(url=url).text))
+
+    metric_df = pd.DataFrame.from_records(data)
+    
+    df_msg = pd.DataFrame(pd.merged(df_time_series,metric_df, how='outer', on='msg_id'))
