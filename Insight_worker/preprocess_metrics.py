0ab2002c55f47144fd03076b9f905f68b39f6107
PRATIK MISHRA <43684300+pratikmishra356@users.noreply.github.com>
2020-03-21 14:59:33 +0530

    Update preprocess_metrics.py

--- Insight_worker/preprocess_metrics.py
+++ Insight_worker/preprocess_metrics.py
@@ -1,1 +1,16 @@
 
+time_series = {'closed-issues-count':['closed-issues-count','date'],
+               'code-changes':['commit_count','date'],
+               'code-changes-lines':['date','added','removed'],
+               'issues-active':['date','issues'],
+               'issues-closed':['date','issues'],
+               'new-contributors':['date','new_contributors'],
+               'reviews':['date','pull_requests']}
+
+Id_value = {'issue-backlog':['issue_id','duration','created_at'],
+           'issue-participants':['issue_id','created_at','participants'],
+           'review-duration':['"pull_request_id','created_at','duration'],
+           'contributors':['user_id','commits','issues','commit_comments',
+                           'issue_comments','pull_requests','pull_request_comments','total']}
+
+text = {'message':['msg_id','cntrb_id','msg_text']} #currently there is no metrics avialable for text.
